04ecef0cd3e22efa0e3138d08eb79e16da7d4aa7
titouancv <titouan.carion.vignaud@gmail.com>
2025-10-08 12:35:16 +0200

    feat: create classe form

--- app/app.vue
+++ app/app.vue
@@ -8,6 +8,48 @@
         <div class="flex flex-col p-2 rounded-xl bg-white"> 
           <div class="flex justify-between w-full">
             <h3>Classes</h3>
+            <div class="flex flex-inline space-x-2 items-center" @click="handleClick">
+              <p v-if="!formData.classe && !formData.bacType">à compléter</p>
+              <p v-else>{{ formData.classe + ", "}}{{ formData.bacType }}</p>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/120/120890.png"
+                class="h-[12px] w-[12px] transition-transform duration-300"
+                :style="{ transform: isClasseOpen ? 'rotate(180deg)' : 'rotate(0deg)' }"
+              />
+            </div>
+          </div>
+          <div v-if="isClasseOpen" class="flex flex-col gap-2">
+            <div class="flex gap-2">
+              <div 
+              v-for="level in ['Seconde', 'Premiere', 'Terminal']" 
+              :key="level"
+              @click="selectClasse(level as ClasseLevel)"
+              class="rounded-full p-2 cursor-pointer transition-colors"
+              :class="tempData.classe === level ? 'bg-gray-900 text-white' : 'bg-gray-300 hover:bg-gray-400'"
+              >
+              {{ level }}
+              </div>
+            </div>
+            <div class="h-px bg-black my-4"></div>
+              <div 
+              v-for="type in ['Générale', 'Technologique', 'Profesionnel']" 
+              :key="type"
+              @click="selectType(type as BacType)"
+              class="rounded-full p-2 cursor-pointer transition-colors"
+              :class="tempData.bacType === type ? 'bg-gray-900 text-white' : 'bg-gray-300 hover:bg-gray-400'"
+              >
+              {{ type }}
+              </div>
+            <button
+              @click="confirmSelection"
+              :disabled="!tempData.classe || !tempData.bacType"
+              class="font-medium px-8 py-3 rounded-full transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+              :class="tempData.classe && tempData.bacType 
+              ? 'bg-gray-900 hover:bg-gray-700 text-white' 
+              : 'bg-gray-300 text-gray-500 cursor-not-allowed'"
+            >
+              Confirmer
+            </button>
           </div>
         </div>
         
@@ -29,8 +71,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-type ClasseLevel = 'Seconde' | 'premiere' | 'terminal'
-type BacType = 'Général' | 'Technologique' | 'Professionel'
+type ClasseLevel = 'Seconde' | 'Premiere' | 'Terminal'
+type BacType = 'Générale' | 'Technologique' | 'Professionnel'
 
 interface Classe {
   classe: ClasseLevel,
@@ -38,7 +80,8 @@ interface Classe {
 }
 
 interface FormData {
-  classe: Classe | null
+  classe: ClasseLevel | null,
+  bacType: BacType | null,
   speciality: string | null
   note: string | null
 }
@@ -47,9 +90,39 @@ const isClasseOpen = ref(false)
 
 const formData = ref<FormData>({
   classe: null,
+  bacType: null,
   speciality: null,
   note: null,
 })
+
+const tempData = ref<FormData>({
+  classe: null,
+  bacType: null,
+  speciality: null,
+  note: null
+})
+
+const selectClasse = (level: ClasseLevel) => {
+  if (tempData.value.classe === level) {
+    tempData.value.classe = null
+  } else {
+    tempData.value.classe = level
+  }
+}
+
+const selectType = (type: BacType) => {
+  if (tempData.value.bacType === type) {
+    tempData.value.bacType = null
+  } else {
+    tempData.value.bacType = type
+  }
+}
+
+const confirmSelection = () => {
+  formData.value.classe = tempData.value.classe
+  formData.value.bacType = tempData.value.bacType
+  isClasseOpen.value = false
+}
 
 const handleClick = () => {
   isClasseOpen.value = !isClasseOpen.value
